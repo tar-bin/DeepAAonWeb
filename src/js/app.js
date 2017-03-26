9e820c5f2c3be0c539fbc7ee4839a31648c1ee2a
tar_bin <tar.bin.master@gmail.com>
2017-03-26 22:37:35 +0900

    画像のサイズ情報を表示したりとか

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 const KerasJS = require('../lib/keras')
 import Vue from 'vue'
+import ndarray from 'ndarray';
+import ops from 'ndarray-ops';
 
 var app = new Vue({
     el: '#app',
@@ -13,40 +15,115 @@ var app = new Vue({
             },
             gpu: true
         }),
-        inputImageURL: 'sample-data/test_image.png',
-        grayscaleImageURL: '',
+        inputImage: {
+            URL: 'sample-data/test_image.png',
+            width: 0,
+            height: 0
+        },
+        grayscaleImage: {
+            URL: '',
+            width: 0,
+            height: 0
+        },
         resultAA: ''
     },
     methods: {
         grayscale: function(imageURL) {
-            var canvas = document.createElement("canvas");
-            var ctx = canvas.getContext("2d");
-            var img = new Image();
-            img.src = imageURL;
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-
-            var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            for (var y = 0; y < pixels.height; y++) {
-                for (var x = 0; x < pixels.width; x++) {
-                    var i = (y * 4) * pixels.width + x * 4;
-                    var rgb = parseInt((pixels.data[i] + pixels.data[i + 1] + pixels.data[i + 2]) / 3, 10);
-                    pixels.data[i] = rgb;
-                    pixels.data[i + 1] = rgb;
-                    pixels.data[i + 2] = rgb;
+            try {
+                // get image
+                var canvas = document.createElement("canvas");
+                var ctx = canvas.getContext("2d");
+                var img = new Image();
+                img.src = imageURL;
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+                var pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                // update input image info
+                this.inputImage.width = img.width;
+                this.inputImage.height = img.height;
+                // grayscaling
+                for (var y = 0; y < pixels.height; y++) {
+                    for (var x = 0; x < pixels.width; x++) {
+                        var i = (y * 4) * pixels.width + x * 4;
+                        var rgb = parseInt((pixels.data[i] + pixels.data[i + 1] + pixels.data[i + 2]) / 3, 10);
+                        pixels.data[i] = rgb;
+                        pixels.data[i + 1] = rgb;
+                        pixels.data[i + 2] = rgb;
+                    }
                 }
+                // update
+                ctx.putImageData(pixels, 0, 0, 0, 0, pixels.width, pixels.height);
+                return [canvas.toDataURL(), pixels.width, pixels.height];
+            } catch (err) {
+                console.error('grayscale: ', err.message);
+                this.resultAA = err.message;
+                return '';
             }
-            ctx.putImageData(pixels, 0, 0, 0, 0, pixels.width, pixels.height);
-            return canvas.toDataURL();
         },
         inputImageLoad: function() {
-            this.grayscaleImageURL = this.grayscale(this.inputImageURL);
+            let [url, width, height] = this.grayscale(this.inputImage.URL);
+            this.grayscaleImage.URL = url;
+            this.grayscaleImage.width = width;
+            this.grayscaleImage.height = height;
         },
-        grayscaleImageLoad: function() {
-            console.log('ready');
-            var out = 'ready';
-            this.resultAA = out;
+        grayscaleImageLoad: async function() {
+            try {
+                // get input data
+                const canvas = document.createElement("canvas");
+                const ctx = canvas.getContext("2d");
+                const img = new Image();
+                img.src = this.grayscaleImage.URL;
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+                const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+                // convert ndarray
+                let dataTensor = ndarray(new Float32Array(pixels.data), [pixels.width, pixels.height]);
+                // normalization (0.0 ~ 1.0)
+                ops.divseq(dataTensor, 255);
+                // calc AA line count
+                let lineNum = Math.floor(pixels.height / 18);
+                console.log('line count', lineNum);
+
+                // wait until model is ready
+                await this.model.ready();
+                console.log('model ready!');
+
+                // eval
+                // for (var i = 0; i < lineNum; i++) {
+                //     console.log(dataTensor)
+
+                //     let lineImage = dataTensor.data.slice(i * 18, i * 18 + 64);
+                //     let lineCharList = '';
+                //     let start = 0;
+                //     let end = 64;
+                //     let penalty = 1;
+
+                //     let width = 64;
+                //     while (end <= dataTensor.shape[1]) {
+                //         // lineImage.T
+                //         let patch = ndarray(new Float32Array(lineImage), [1, 64, 64]);
+                //         // predict
+                //         const inputData = {
+                //             'input_1': new Float32Array(patch.data)
+                //         };
+                //         const outputData = await this.model.predict(inputData);
+                //         console.log(outputData);
+
+                //         end += width;
+                //         await this.sleep(100);
+                //         console.log('processing: ', i, ', ', end);
+                //     }
+                // }
+            } catch (err) {
+                console.error('model: ', err.message);
+                this.resultAA = err.message;
+            }
+        },
+        sleep: async function (ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
         }
     },
     computed: {
