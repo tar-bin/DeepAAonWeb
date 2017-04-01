362134b92fcd6d6bb506782a76d9f4ef79f3f396
tar_bin <tar.bin.master@gmail.com>
2017-04-01 18:51:18 +0900

    クリップボードにコピーボタンの追加

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,31 @@
 // JS Module
 import * as KerasJS from 'keras-js'
-import Vue from 'vue'
 import ndarray from 'ndarray'
 import ops from 'ndarray-ops'
+import Vue from 'vue'
+import Clipboard from 'clipboard'
+
 // JSON
 import CharListFile from  '../char_list.json'
 
-var app = new Vue({
+// clipboard.js
+var clipboard = new Clipboard('.btn');
+
+clipboard.on('success', function (e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function (e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+// Vue.js
+new Vue({
     el: '#app',
     data: {
         modelLoading: true,
