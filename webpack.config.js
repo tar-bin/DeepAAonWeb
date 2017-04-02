bdf193520ce267f0010ff48ce1628d00d75b71fe
tar_bin <tar.bin.master@gmail.com>
2017-04-02 22:55:50 +0900

    babelでのes5への変換に対応

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
                 test: /\.json$/,
                 use: 'json-loader'
             }
