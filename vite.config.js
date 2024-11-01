86180e608d60774493a4dea6a5d78c1fb7f295aa
tar_bin <tar.bin.master@gmail.com>
2024-11-02 02:11:27 +0900

    リソースを相対パスに

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   build: {
     outDir: 'docs',
-    assetsDir: "."
   }
 })
